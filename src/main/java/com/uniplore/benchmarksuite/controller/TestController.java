48e7e6f27bb388f76dd5aac2403cf800d7463b1f
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-03-17 16:46:11 +0800

    ssm整合完成

---
+++ src/main/java/com/uniplore/benchmarksuite/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.uniplore.benchmarksuite.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+   
+	@RequestMapping(value="/Test")
+	public String toIndex (){
+		System.out.println("maven测试");
+        return "test";  
+	}
+}
