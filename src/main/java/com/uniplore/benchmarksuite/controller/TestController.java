dffa8d265cd63a29f00c16fe9b1f19892aef0c42
zhujunpengguizhou <gzuzhujunpeng@gmail.com>
2017-03-18 10:34:30 +0800

    修改配置信息

--- src/main/java/com/uniplore/benchmarksuite/controller/TestController.java
+++ src/main/java/com/uniplore/benchmarksuite/controller/TestController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
    
-	@RequestMapping(value="/Test")
-	public String toIndex (){
-		System.out.println("maven测试");
-        return "test";  
-	}
+  @RequestMapping(value = "/Test")
+  public String toIndex() {
+    System.out.println("maven测试");
+    return "test";  
+  }
 }
